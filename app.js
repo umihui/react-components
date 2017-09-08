0ac78f04f9282abf1961c276b9256e81f0d808f6
umihui <umihui86@gmail.com>
2017-09-08 12:04:47 -0700

    complete

--- app.js
+++ app.js
@@ -6,11 +6,25 @@ class GroceryListItem extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      done : false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
   }
 
   render() {
+    var style = {
+      'font-weight': this.state.done ? 'bold' : 'normal'
+    };
+
     return (
-      <li>{this.props.item}</li>
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
     );
   }
 
@@ -30,61 +44,4 @@ var App = () => (
   </div>
 );
 
-// var GroceryListItem = (props) => (
-//   <ul>
-//     <li>{props.todos[0]}</li>
-//     <li>{props.todos[1]}</li>
-//     <li>{props.todos[2]}</li>
-//   </ul>
-// );
-
-//ReactDOM.render(<GroceryList />, document.getElementById("app"));
-
-
-// A class component can be defined as an ES6 class
-// that extends the base Component class included in the React library
-// class TodoListItem extends React.Component {
-//
-//   // A `constructor` method is expected on all ES6 classes
-//   // When React instantiates the component,
-//   // it will pass `props` to the constructor
-//   constructor(props) {
-//     // Equivalent to ES5's React.Component.call(this, props)
-//     super(props);
-//     //console.log(props);
-//   }
-//   onListItemClick(event) {
-//       console.log('I got clicked');
-//     };
-//   // Every class component must have a `render` method
-//   // Stateless functional components are pretty much just this method
-//   render() {
-//
-//     // `props` is no longer passed as an argument,
-//     // but instead accessed with `this.props`
-//     return (
-//       <li onClick={this.onListItemClick}>{this.props.todo}</li>
-//     );
-//
-//   }
-//
-// }
-//
-// // Update our `TodoList` to use the new `TodoListItem` component
-// // This can still be a stateless function component!
-// var TodoList = (props) => (
-//   <ul>
-//     {props.todos.map(todo =>
-//       <TodoListItem todo={todo} />
-//     )}
-//   </ul>
-// );
-//
-// var App = () => (
-//   <div>
-//     <h2>My Todo List</h2>
-//     <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep','clean' ,'cat']}/> // Here we are creating an instance of the `TodoList` component
-//   </div>
-// );
-//
 ReactDOM.render(<App />, document.getElementById("app"));
